b2a38505e3c9f8ff2eb2768e4e016a64b0bf1ab3
DAPSE <dapsegura@gmail.com>
2026-04-18 22:26:04 -0600

    .

--- 00_Apuntes_Python/001_Conceptos_Basicos/Apuntes.py
+++ 00_Apuntes_Python/001_Conceptos_Basicos/Apuntes.py
@@ -67,34 +67,3 @@ nombre_completo = nombre1
 nombre_completo += nombre2
 
 print("\n7)Me llamo", nombre_completo) #Impresion concatenando las variables
-
-##Ejercicio 1.
-
-
-
-
-## Ejercicio 2.
-first_name = 'John'
-last_name = 'Doe'
-full_name = first_name + ' ' + last_name
-address = '123 Main Street'
-address += ', Apartment 4B'
-employee_age = 28
-employee_info = full_name + ' is ' + str(employee_age) + ' years old'
-print(employee_info)
-experience_years = 5
-experience_info = 'Experience: ' + str(experience_years) + ' years'
-print(experience_info)
-position = 'Data Analyst'
-salary = 75000
-employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
-print(employee_card)
-employee_code = 'DEV-2026-JD-001'
-department = employee_code[0:3]
-print(department)
-year_code = employee_code[4:8]
-print(year_code)
-initials = employee_code[9:11]
-print(initials)
-last_three = employee_code[-3:]
-print(last_three)
